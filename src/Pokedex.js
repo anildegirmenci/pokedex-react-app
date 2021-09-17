0138f348294dcabe4e382b7df2c65dc3f9f152ed
anildegirmenci <anildegirmenci71@gmail.com>
2021-09-17 03:59:26 +0300

    Svg added, other implementations has been made.

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,6 +2,7 @@ import  React, { useState } from 'react';
 import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import getData from './getData';
+import Pokeball from './Pokeball';
 
 
 
@@ -13,33 +14,38 @@ const useStyles = makeStyles({
     }
 });
 
-const getPokemonCard = () => {
-    return(
-    <Grid item xs={12} sm={4}>
-        <Card>
-            <CardContent>
-                Hi!
-            </CardContent>
-        </Card>
-    </Grid>
-    )
-}
 
 const Pokedex = () =>{
     const classes = useStyles();
     const [pokemonData, setPokemonData] = useState(getData);
+
+    const getPokemonCard = (pokemonId) => {
+        console.log(pokemonData[`${pokemonId}`]);
+        return(
+        <Grid item xs={12} sm={4} key={pokemonId}>
+            <Card>
+                <CardContent>
+                    Hi!
+                </CardContent>
+            </Card>
+        </Grid>
+        )
+    }
     return(
         <>
             <AppBar position = "static">
                 <Toolbar />
             </AppBar>
-
-            <Grid container spacing = {2} className = {classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
+            {pokemonData ? (
+                <Grid container spacing = {2} className = {classes.pokedexContainer}>
+                {Object.keys(pokemonData).map((pokemonId) => {
+                    getPokemonCard(pokemonId)
+                })}
             </Grid>
+            ): (
+                <Pokeball />
+            )}
+            
         </>
     )
 }
